de492b4a575bfd41ea101c6850bab1585724be9e
Stephen <stephengift43@gmail.com>
2022-11-12 22:32:55 +0100

    Page routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import AllMeetUps from "./Pages/AllMeetUps";
+import Favourites from "./Pages/Favourites";
+import NewMeetUps from "./Pages/NewMeetUps";
+
 function App() {
   return (
-    <div>hi</div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<AllMeetUps />} />
+        <Route exact path="/new-meetup" element={<NewMeetUps />} />
+        <Route exact path="/favourites" element={<Favourites />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
