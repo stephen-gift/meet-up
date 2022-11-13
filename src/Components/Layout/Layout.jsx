74c6c3a156436cb708f6f6617fdb26bfec768cf0
Stephen <stephengift43@gmail.com>
2022-11-13 16:24:46 +0100

    Card wrapper added

---
+++ src/Components/Layout/Layout.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./Layout.module.css";
+
+const Layout = () => {
+  return <div>Layout</div>;
+};
+
+export default Layout;
--- src/Components/Meetups/MeetupItem.jsx
+++ src/Components/Meetups/MeetupItem.jsx
@@ -1,20 +1,23 @@
 import React from "react";
+import Card from "../Ui/Card";
 import classes from "./MeetupItem.module.css";
 
 const MeetupItem = (props) => {
   return (
     <li className={classes.item}>
-      <div className={classes.image}>
-        <img src={props.image} alt="" />
-      </div>
-      <div className={classes.content}>
-        <h3 >{props.title}</h3>
-        <address>{props.address}</address>
-        <p>{props.description}</p>
-      </div>
-      <div className={classes.actions}>
-        <button>To Favourites</button>
-      </div>
+      <Card>
+        <div className={classes.image}>
+          <img src={props.image} alt="" />
+        </div>
+        <div className={classes.content}>
+          <h3>{props.title}</h3>
+          <address>{props.address}</address>
+          <p>{props.description}</p>
+        </div>
+        <div className={classes.actions}>
+          <button>To Favourites</button>
+        </div>
+      </Card>
     </li>
   );
 };
