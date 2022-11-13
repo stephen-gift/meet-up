2968b5f8226bd3909b476a904107949f2fded449
Stephen <stephengift43@gmail.com>
2022-11-14 00:44:48 +0100

    Sending Post request
    Activating the app to send request to FIREBASE

--- src/Pages/NewMeetUps.jsx
+++ src/Pages/NewMeetUps.jsx
@@ -2,9 +2,17 @@ import React from "react";
 import NewMeetupForm from "../Components/Meetups/NewMeetupForm";
 
 const NewMeetUps = () => {
-const addMeetupHandler=(meetupData)=>{
-  
-}
+  const addMeetupHandler = (meetupData) => {
+    const URL =
+      "https://reactprojecttest1-default-rtdb.firebaseio.com/meetups.json";
+    fetch(URL, {
+      method: "POST",
+      body: JSON.stringify(meetupData),
+      header: {
+        "Content-Type": "application/json",
+      },
+    });
+  };
 
   return (
     <section>
