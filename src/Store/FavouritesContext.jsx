0114d9c14f49ee86209ce6ad1a2ffb91b5c76c8c
Stephen <stephengift43@gmail.com>
2022-11-14 15:29:52 +0100

    setting up react context

---
+++ src/Store/FavouritesContext.jsx
@@ -0,0 +1,23 @@
+import React, { createContext, useState } from "react";
+
+const FavouritesContext = createContext({
+  favourites: [],
+  totalFavourites: 0,
+});
+
+const FavouritesContextProvider = (props) => {
+  const [userFavourites, setUserFavourites] = useState([]);
+
+  const context = {
+    favourites: userFavourites,
+    totalFavourites: userFavourites.length,
+  };
+
+  return (
+    <FavouritesContext.Provider value={context}>
+      {props.children}
+    </FavouritesContext.Provider>
+  );
+};
+
+export default FavouritesContextProvider;
