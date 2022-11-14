17c3cb857edebd832b01671dcff52601eca0287a
Stephen <stephengift43@gmail.com>
2022-11-14 18:52:06 +0100

    Outputting favourites to the Favourites page

--- src/Components/Layout/MainNavigation.jsx
+++ src/Components/Layout/MainNavigation.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import FavouritesContext from "../../Store/FavouritesContext";
 import classes from './MainNavigation.module.css'
 
 const MainNavigation = () => {
+  const favouritesCtx = useContext(FavouritesContext)
   return (
     <header className={classes.header}>
       <div className={classes.logo}>React Meetups</div>
@@ -15,7 +17,8 @@ const MainNavigation = () => {
             <Link to="/new-meetup">Add New Meetup</Link>
           </li>
           <li>
-            <Link to="/favourites">My Favourites</Link>
+            <Link to="/favourites">My Favourites
+            <span className={classes.badge}>{favouritesCtx.totalFavourites}</span></Link>
           </li>
         </ul>
       </nav>
--- src/Pages/Favourites.jsx
+++ src/Pages/Favourites.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useContext } from "react";
+import MeetUpList from "../Components/Meetups/MeetUpList";
+import FavouritesContext from "../Store/FavouritesContext";
 
 const Favourites = () => {
-  return (
-    <div>Favourites</div>
-  )
-}
+  const favouritesCtx = useContext(FavouritesContext);
+  let content;
 
-export default Favourites
+  if (favouritesCtx.totalFavourites === 0) {
+    content = <p>You have no Favourites yet</p>;
+  } else {
+    content = <MeetUpList meetups={favouritesCtx.favourites} />;
+  }
+  return (
+    <section>
+      <h1>Favourites</h1>
+      {content}
+    </section>
+  );
+};
+
+export default Favourites;
